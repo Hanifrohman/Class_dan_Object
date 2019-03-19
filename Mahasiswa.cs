53990b4a4a8b85bf166a912af83d9a64bd8de04d
Hanifrohman <hanifrohman17@gmail.com>
2019-03-19 12:15:27 +0700

    Program.cs

---
+++ Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_class_dan_object
+{
+    class Mahasiswa
+    {
+        //prperties
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+        public float IPK { get; set; }
+
+        //method
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM : {0}", NIM);
+            Console.WriteLine("IPK : {0}", IPK);
+            Console.WriteLine("Telah melakukan regristasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses KRS\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_class_dan_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //mengeset nilai prperti objek saya dan kamu
+            saya.NIM = "12345";
+            saya.Nama = "Hanif";
+            saya.IPK = 3.95f;
+
+            kamu.NIM = "123456";
+            kamu.Nama = "Hanas";
+            kamu.IPK = 2.70f;
+
+            //memanggil method regristasi dan isi KRS
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
